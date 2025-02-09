b0c8f55357670180ca3218252f359c11bfcb7f23
Incandescent-pixel <drichards1712@gmail.com>
2025-02-09 18:12:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    // This function is only available to members
+    // It will only be called when the member is logged in
+    // So no need to check if the member is logged in
+    // Just do the stuff
+}
